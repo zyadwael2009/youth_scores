f4cfca511140fc99385643876d06e097303106e4
zantac <zantac@users.noreply.github.com>
2026-09-16 09:50:15 +0000

    deploy: b646c9ff7ee66c4e520a9df8df352af4ebb48132

---
+++ _next/static/Fa8KQDgNorClhVPITyuOb/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fclub\u002F[id]","\u002Fcoach\u002F[id]","\u002Fcompetition\u002F[id]","\u002Fmatch\u002F[id]","\u002Fplayer\u002F[id]","\u002Fteam\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
